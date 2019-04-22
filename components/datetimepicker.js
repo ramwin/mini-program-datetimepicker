33ad318c6734e2b7ff83010de8fd7b4f67760d1e
Xiang Wang <ramwin@qq.com>
2019-04-22 18:27:04 +0800

    初始化选择的时间

--- components/datetimepicker.js
+++ components/datetimepicker.js
@@ -1,5 +1,4 @@
 // components/datetimepicker.js
-var now = new Date();
 function get_initial_range() {
   var YEAR_RANGE = []
   for (var i=2018; i<=2021; i++) {
@@ -31,7 +30,7 @@ function formattime(date) {
 function date2object(date) {
   return {
     "year": date.getFullYear(),
-    "month": date.getMonth(),
+    "month": date.getMonth() + 1,
     "day": date.getDate(),
     "hour": date.getHours(),
     "minute": date.getMinutes(),
@@ -39,40 +38,50 @@ function date2object(date) {
   }
 }
 
-const INIT_CONFIG = {
-  placeholder: "请选择开始时间",
-  init_datetime: now,
+function range_to_value(range, selected_value) {
+  // 找到range里面每一列对应的value
+  var result = [ ]
 }
 
 Component({
   properties: {
     config: {
       type: Object,
-      value: INIT_CONFIG,
       observer: function(newVal, oldVal) {
-        // console.info("传入了新的配置", newVal);
-        this.setData({
-          "config_inner": newVal,
-        })
-        if (newVal.placeholder) {
+        console.info("传入了新的配置", newVal);
+        if (!newVal.init_datetime) {
+          newVal.init_datetime = new Date();
         }
+        if (!newVal.placeholder) {
+          newVal.placeholder = "请选择开始时间"
+        }
+        this.setData({
+          "range": get_initial_range(),
+          "placeholder": newVal.placeholder,
+          "date": newVal.init_datetime,
+        })
+        this.update_value();
       }
     },
   },
   data: {
-    range: get_initial_range(),
-    date_str: undefined,
-    date: undefined,
-    config_inner: INIT_CONFIG,
-    selected_value: date2object(now),  // 仅仅是选择的，但是没有确定
-    column_match: {
+    range: undefined,  // 选择的范围
+    date_str: undefined,  // 选择的时间的展示内容, 如果为空，说明还没有选择, 否则就是选择或者传入了初始时间
+    date: undefined,  // 选择的时间, 必定存在
+    placeholder: undefined,  // 如果没有选择时间，显示的内容
+    selected_value: [0,0,0,0,0],  // 仅仅是选择的每列的index，但是没有确定
+    column_match: {  // 每一列对应的名字
       "0": "year",
       "1": "month",
       "2": "day",
       "3": "hour",
       "4": "minute",
-      "5": "second",
     },
+  },
+  lifetimes: {
+    attached: function() {
+      console.info("载入了初始组件");
+    }
   },
   methods: {
     bindchange: function(res) {
@@ -92,14 +101,49 @@ Component({
       })
     },
     bindcolumnchange: function(res) {
-      // console.info("修改了一列", res);
-      var name = this.data.column_match[res.detail.column];
-      this.data.selected_value[name] = this.data.range[
-        res.detail.column][res.detail.value]["value"]
-      // console.info(this.data.selected_value)
+      console.info("修改了一列", res);
+      this.data.selected_value[res.detail.column] = res.detail.value;
       this.setData({
         "selected_value": this.data.selected_value,
       })
     },
+    update_value: function() {
+      console.info("根据当前选择的date，设置选择好的时间");
+      var scope = this;
+      for (var index_str in this.data.column_match) {
+        var index = parseInt(index_str)
+        var name = this.data.column_match[index_str];
+        var value = 0;
+        switch (name) {
+          case "year":
+            value = this.data.date.getFullYear();
+            break;
+          case "month":
+            value = this.data.date.getMonth() + 1;
+            break;
+          case "day":
+            value = this.data.date.getDate();
+            break;
+          case "hour":
+            value = this.data.date.getHours();
+            break;
+          case "minute":
+            value = this.data.date.getMinutes();
+            break;
+          default:
+            continue;
+        }
+        this.data.range[index].forEach(
+          function(item, index2, value2) {
+            if (item.value == value) {
+              scope.data.selected_value[index] = index2;
+            }
+          }
+        )
+      }
+      this.setData({
+        "selected_value": this.data.selected_value,
+      })
+    }
   }
 })
